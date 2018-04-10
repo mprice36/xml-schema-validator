7edcc7d0520c6cc343a8c914d3cc51f17015fd16
mprice36 <miles.price@gtri.gatech.edu>
2018-04-10 15:51:21 -0400

    Code Cleanup
    Did code clean up that does not change the underlying code but rearranges the code for readability and removed dependencies that are not used or contribute to the final goal of the project.

--- src/main/java/org/gtri/niem/xml_schema_validator/Logger.java
+++ src/main/java/org/gtri/niem/xml_schema_validator/Logger.java
@@ -1,88 +1,89 @@
-
 package org.gtri.niem.xml_schema_validator;
 
-import org.slf4j.Marker;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
 public class Logger extends org.slf4j.ext.LoggerWrapper {
+    private static Logger logger = new Logger();
+    //////////////////////////////////////////////////////////////////
+    // remember exit status
+    private int exitStatus = 0;
 
-  //////////////////////////////////////////////////////////////////
-  // singleton
-  private Logger() {
-    super(LoggerFactory.getLogger(Logger.class), Logger.class.getName());
-  }
-  private static Logger logger = new Logger();
-  public static Logger getInstance() {
-    return logger;
-  }
+    //////////////////////////////////////////////////////////////////
+    // singleton
+    private Logger() {
+        super(LoggerFactory.getLogger(Logger.class), Logger.class.getName());
+    }
 
-  //////////////////////////////////////////////////////////////////
-  // remember exit status
-  private int exitStatus = 0;
-  public void setExitStatus(int status) {
-    exitStatus = status;
-  }
-  public int getExitStatus() {
-    return exitStatus;
-  }
+    public static Logger getInstance() {
+        return logger;
+    }
 
-  //////////////////////////////////////////////////////////////////
-  // what to do if there is an error
-  private void onError() {
-    logger.info("Exiting due to error");
-    System.exit(1);
-  }
+    public int getExitStatus() {
+        return exitStatus;
+    }
 
-  //////////////////////////////////////////////////////////////////
-  // override of LoggerWrapper
-    
-  public void error(String msg) {
-    super.error(msg);
-    onError();
-  }
-      
-  public void error(String format, Object arg) {
-    super.error(format, arg);
-    onError();
-  }
+    public void setExitStatus(int status) {
+        exitStatus = status;
+    }
 
-  public void error(String format, Object arg1, Object arg2) {
-    super.error(format, arg1, arg2);
-    onError();
-  }
+    //////////////////////////////////////////////////////////////////
+    // what to do if there is an error
+    private void onError() {
+        logger.info("Exiting due to error");
+        System.exit(1);
+    }
 
-  public void error(String format, Object... args) {
-    super.error(format, args);
-    onError();
-  }
+    //////////////////////////////////////////////////////////////////
+    // override of LoggerWrapper
 
-  public void error(String msg, Throwable t) {
-    super.error(msg, t);
-    onError();
-  }
+    public void error(String msg) {
+        super.error(msg);
+        onError();
+    }
 
-  public void error(Marker marker, String msg) {
-    super.error(marker, msg);
-    onError();
-  }
+    public void error(String format, Object arg) {
+        super.error(format, arg);
+        onError();
+    }
 
-  public void error(Marker marker, String format, Object arg) {
-    super.error(marker, format, arg);
-    onError();
-  }
+    public void error(String format, Object arg1, Object arg2) {
+        super.error(format, arg1, arg2);
+        onError();
+    }
 
-  public void error(Marker marker, String format, Object arg1, Object arg2) {
-    super.error(marker, format, arg1, arg2);
-    onError();
-  }
+    public void error(String format, Object... args) {
+        super.error(format, args);
+        onError();
+    }
 
-  public void error(Marker marker, String format, Object... args) {
-    super.error(marker, format, args);
-    onError();
-  }
+    public void error(String msg, Throwable t) {
+        super.error(msg, t);
+        onError();
+    }
 
-  public void error(Marker marker, String msg, Throwable t) {
-    super.error(marker, msg, t);
-    onError();
-  }
+    public void error(Marker marker, String msg) {
+        super.error(marker, msg);
+        onError();
+    }
+
+    public void error(Marker marker, String format, Object arg) {
+        super.error(marker, format, arg);
+        onError();
+    }
+
+    public void error(Marker marker, String format, Object arg1, Object arg2) {
+        super.error(marker, format, arg1, arg2);
+        onError();
+    }
+
+    public void error(Marker marker, String format, Object... args) {
+        super.error(marker, format, args);
+        onError();
+    }
+
+    public void error(Marker marker, String msg, Throwable t) {
+        super.error(marker, msg, t);
+        onError();
+    }
 }
